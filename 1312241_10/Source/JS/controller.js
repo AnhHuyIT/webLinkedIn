8c6df674465d203ed9cba340721c9de3eb14b261
AnhHuyIT <nguyenanhhuyit@gmail.com>
2016-05-05 06:09:30 +0700

    version 2.1 Update json, thuc hien yeu cau 4

--- 1312241_10/Source/JS/controller.js
+++ 1312241_10/Source/JS/controller.js
@@ -1,7 +1,24 @@
 var Background = angular.module('Background', ['ngSanitize']);
 
-Background.controller('BackgroundCtrl', function ($scope, $http) {
+Background.controller('BackgroundCtrl', ['$scope','$http', function ($scope, $http) {
   $http.get('JS/background.json').success(function(data) {
     $scope.background = data;
   });
-});
+    
+    $scope.edit = {statusfullname:{name:"hello", status:false},
+                    statusjob:{name:"hello",  status:false},
+                    statushistory:{name:"hello",  status:false},
+                    statusinformationme:{name:"hello",  status:false},
+                    statesummary:{name:"hello",  status:false},
+                    stateeducation:{name:"hello",  status:false},
+                    stateproject:{name:"hello",  status:false},
+                    stateavatar:{name:"hello",  status:false},
+                    stateskill:{name:"hello",  status:false}
+           				};
+
+            $scope.hidetextbox = function(nameshow){
+            	nameshow.status = !nameshow.status;
+              console.log(nameshow);   
+            };
+      
+}]);
